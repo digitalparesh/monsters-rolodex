7f133748e210a5b7a4dd4cc6f3de0aabd7801dc8
digitalparesh <prshnandaniya@gmail.com>
2020-09-22 13:24:24 +0530

    Updates

---
+++ precache-manifest.c1f68038b7c269d6bc8cae998ad114ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfcc29e394d2592b5caf7ed306c1e1a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "098f1f89833d09df3d87",
+    "url": "/monsters-rolodex/static/css/main.6ee7c3f6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098f1f89833d09df3d87",
+    "url": "/monsters-rolodex/static/js/main.67b5f000.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
